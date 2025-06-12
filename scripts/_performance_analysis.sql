dd97629423dd2b1070a91a272bba6ceeda409ad5
avanibhatt <42451708+avanibhatt@users.noreply.github.com>
2025-06-12 10:37:15 -0400

    Update _performance_analysis.sql

--- scripts/_performance_analysis.sql
+++ scripts/_performance_analysis.sql
@@ -1,21 +1,36 @@
---Calculate the total sales per month
--- and the running total of zsales over time
--- And the moving average of sales over time
+/* Analyze the yearly performance of products by comparing their sales to both
+the average sales performance of the product and the previous year's sales */
 
-SELECT 
-order_date,
-total_sales,
-SUM(total_sales) OVER(ORDER BY order_date) AS running_total_sales,
-AVG(avg_sales) OVER(ORDER BY order_date) AS moving_avg_price
-FROM(
-	SELECT
-	--DATETRUNC(MONTH,order_date) as order_date,
-	DATEFROMPARTS(YEAR(order_date),'01','01') AS order_date,		-- FOR NEW VERSION USE DATETRUNC FUNCTION INSTEAD
-	--DATEFROMPARTS(YEAR(order_date),MONTH(order_date),'01') AS order_date,
-	SUM(sales_amount)  AS total_sales,
-	AVG(sales_amount) AS avg_sales
-	FROM gold.fact_sales
-	WHERE order_date IS NOT NULL
-	GROUP BY DATEFROMPARTS(YEAR(order_date),'01','01')--FORMAT(order_date,'yyyy-MM-01')	--DATETRUNC(MONTH,order_date)
-	   --DATETRUNC(MONTH,order_date)	
-)t
+WITH yearly_product_sales AS(
+SELECT
+	YEAR(f.order_date) AS order_year,
+	p.product_name,
+	SUM(f.sales_amount) AS current_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key 
+WHERE f.order_date IS NOT NULL
+GROUP BY 
+	YEAR(f.order_date),
+	p.product_name
+)
+
+SELECT
+order_year,
+product_name,
+current_sales,
+AVG(current_sales) OVER(PARTITION BY product_name) AS avg_sales,
+current_sales - AVG(current_sales) OVER(PARTITION BY product_name)  AS diff_avg,
+CASE WHEN current_sales - AVG(current_sales)OVER(PARTITION BY product_name) > 0 THEN 'Above Average'
+	WHEN current_sales - AVG(current_sales)OVER(PARTITION BY product_name) < 0  THEN 'Below Average'
+	ELSE 'Average'
+END AS avg_change,
+--YEAR OVER YEAR ANALYSIS
+LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) py_sales,
+current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) AS diff_py,
+CASE WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
+	WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) < 0  THEN 'Decrease'
+	ELSE 'No Change'
+END AS py_change
+FROM yearly_product_sales
+ORDER BY product_name, order_year
